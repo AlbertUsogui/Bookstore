ed5e0841b580351de63633300733583fe03f2013
AlbertUsogui <albertgeraldo899@gmail.com>
2025-03-09 15:28:20 +0700

    1.0

---
+++ finalproject/script.js
@@ -0,0 +1,96 @@
+function data(){
+     window.location.href = "../bookstore/bookstore.html"
+}
+
+function data1(){
+    window.location.href = "../Orange/index.html"
+}
+
+val = document.getElementById("none");    
+span = document.getElementById("close");
+btn = document.getElementById("mybtn");
+btn.onclick = function() {
+  val.style.display = "block";
+  }
+  span.onclick = function() {
+      val.style.display = "none";
+  }
+  
+
+      dav = document.getElementById("none1");    
+      span1 = document.getElementById("close1");
+      btn1 = document.getElementById("mybtn1");
+      btn1.onclick = function() {
+        dav.style.display = "block";
+        }
+        span1.onclick = function() {
+            dav.style.display = "none";
+        }
+        
+      
+        har1 = document.getElementById("none2");    
+        span2 = document.getElementById("close2");
+        btn2 = document.getElementById("mybtn2");
+        btn2.onclick = function() {
+            har1.style.display = "block";
+          }
+          span2.onclick = function() {
+            har1.style.display = "none";
+          }
+          
+          mic = document.getElementById("none3");    
+          span3 = document.getElementById("close3");
+          btn3 = document.getElementById("mybtn3");
+          btn3.onclick = function() {
+            mic.style.display = "block";
+            }
+            span3.onclick = function() {
+                mic.style.display = "none";
+            }
+
+
+         sam = document.getElementById("none5");    
+        span5 = document.getElementById("close5");
+        btn5 = document.getElementById("mybtn5");
+        btn5.onclick = function() {
+            sam.style.display = "block";
+          }
+          span5.onclick = function() {
+            sam.style.display = "none";
+              }
+              alb = document.getElementById("none4");    
+              span4 = document.getElementById("close4");
+              btn4 = document.getElementById("mybtn4");
+              btn4.onclick = function() {
+                  alb.style.display = "block";
+                }
+                span4.onclick = function() {
+                  alb.style.display = "none";
+                    }
+              ch = document.getElementById("none6");    
+              span6 = document.getElementById("close6");
+              btn6 = document.getElementById("mybtn6");
+              btn6.onclick = function() {
+                ch.style.display = "block";
+                }
+                span6.onclick = function() {
+                    ch.style.display = "none";
+                    }
+             sal = document.getElementById("none7");    
+                span7 = document.getElementById("close7");
+                btn7 = document.getElementById("mybtn7");
+                btn7.onclick = function() {
+                    sal.style.display = "block";
+                  }
+                  span7.onclick = function() {
+                    sal.style.display = "none";
+                      }
+            lin = document.getElementById("none8");    
+          span8 = document.getElementById("close8");
+          btn8 = document.getElementById("mybtn8");
+               btn8.onclick = function() {
+                lin.style.display = "block";
+                   }
+                 span8.onclick = function() {
+                    lin.style.display = "none";
+                          }
