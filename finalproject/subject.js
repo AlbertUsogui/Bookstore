cd643fab051b82dfebb0139f2946efc4c919fa0b
AlbertUsogui <albertgeraldo899@gmail.com>
2025-03-15 14:35:19 +0700

    second commit

--- finalproject/subject.js
+++ finalproject/subject.js
@@ -6,3 +6,11 @@ function student(){
 function teacher(){
     window.location.href = "../finalproject/teacher.html"
 }
+
+function data(){
+    window.location.href = "../bookstore/bookstore.html"
+}
+
+function data1(){
+   window.location.href = "../Orange/index.html"
+}
